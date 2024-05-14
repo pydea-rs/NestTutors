f9f89e2ac5c413328a220429bbdc04acd0dbf9ab
pya-h <p4ya.gcc@gmail.com>
2024-05-15 01:27:58 +0330

    descring

--- 04-used-car-pricing/src/user/auth.service.spec.ts
+++ 04-used-car-pricing/src/user/auth.service.spec.ts
@@ -1,23 +1,31 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
-it('can create a instance of AuthService', async () => {
-  const fakeUserService = {
-    find: () => Promise.resolve([]),
-    create: (username: string, email: string, password: string) =>
-      Promise.resolve({ id: 1, username, email, password }),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UserService,
-        useValue: fakeUserService,
-      },
-    ],
-  }).compile();
+describe('UnitTesting AuthService:', () => {
+  let service: AuthService;
 
-  const service = module.get(AuthService);
-  expect(service).toBeDefined();
+  beforeEach(async () => {
+    const fakeUserService: Partial<UserService> = {
+      find: () => Promise.resolve([]),
+      create: (username: string, email: string, password: string) =>
+        Promise.resolve({ id: 1, username, email, password } as User),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create a instance of AuthService', async () => {
+    expect(service).toBeDefined();
+  });
 });
